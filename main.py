932305a0cf3f155d4f969fe0375872d30abbff97
vdamasceno04 <vdamasceno04@gmail.com>
2024-10-21 22:50:11 -0300

    parse args and generate test data

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from argparse import ArgumentParser
+import re
+import random
+import time
 
 def parse_args():
     parser = ArgumentParser(description = 'Knapsack Problem')
@@ -23,6 +26,52 @@ def parse_args():
 def main():
     args = parse_args()
     print(args)
+    if(args.values is not None and args.weights is not None and args.W is not None):
+        valueList = [int(s) for s in re.findall(r'\b\d+\b', args.values)]
+        weightList = [int(s) for s in re.findall(r'\b\d+\b', args.weights)]
+
+        if len(valueList) != args.totalItems or len(weightList) != args.totalItems:
+            print("ERROR: Number of items in values and weights must be equal to totalItems")
+            return
+
+        if args.W <= 0:
+            print("ERROR: Capacity must be greater than zero")
+            return
+
+        capacity = args.W
+
+    else:
+        if args.seed is not None:
+            random.seed(args.seed)
+        valueList = random.sample(range(1, 9999), int(args.totalItems))
+        weightList = random.sample(range(1, 9999), int(args.totalItems))
+
+        if args.W is not None:
+            capacity = args.W
+        else:
+            capacity = random.randint(1, 99999)
+
+    print(valueList)
+    print(weightList)
+    print(capacity)
+
+    if args.algorithm == 'random':
+        start = time.time()
+        #randomAlgorithm()
+        end = time.time()
+        print('Elapsed time random: ')
+
+    elif args.algorithm == 'annealing':
+        start = time.time()
+        #annealingAlgorithm()
+        end = time.time()
+        print('Elapsed time annealing: ')
+
+    elif args.algorithm == 'genetic':
+        start = time.time()
+        #geneticAlgorithm()
+        end = time.time()
+        print('Elapsed time genetic: ')
 
 if __name__ == '__main__':
     main() 
