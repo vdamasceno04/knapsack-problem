35056ec5743926e1ea704bd377b62ce36961b0be
oaldsp <otaviopepe@alunos.utfpr.edu.br>
2024-11-04 00:25:40 -0300

    graph 1000exec flag true adition

--- main_script.py
+++ main_script.py
@@ -4,7 +4,7 @@ import subprocess
 language = 'python3'
 file = 'main.py'
 itens_number = '20'
-execution_number = '3'
+execution_number = '1000'
 seed = '10'
 
 def execute_annealing(t_selector, random_start_flag):
@@ -12,9 +12,9 @@ def execute_annealing(t_selector, random_start_flag):
     subprocess.run(comand) 
     print(f"========================================================================\n  {' '.join(comand)} - EXECUTED\n========================================================================\n")
 
-#execute_annealing('0', 'False')
+execute_annealing('0', 'False')
 execute_annealing('1', 'False')
-#execute_annealing('2', 'False')
+execute_annealing('2', 'False')
 #execute_annealing('0', 'True')
 #execute_annealing('1', 'True')
 #execute_annealing('2', 'True')
