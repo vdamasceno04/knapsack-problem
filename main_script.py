5e31475f5f99977a70fc8cc4d9d2ca87ed9a4d1a
oaldsp <otaviopepe@alunos.utfpr.edu.br>
2024-11-04 22:09:43 -0300

    genetic script addition.

--- main_script.py
+++ main_script.py
@@ -15,6 +15,11 @@ def execute_annealing(t_selector, random_start_flag):
     subprocess.run(comand) 
     print(f"========================================================================\n  {' '.join(comand)} - EXECUTED\n========================================================================\n")
 
+def execute_genetic(populationSize, numGenerations, mutationRate):
+    comand = [language, file, 'genetic', itens_number, '-seed', seed,  '-ex', execution_number, '-ps', populationSize, '-ng', numGenerations, '-mr', mutationRate]
+    subprocess.run(comand) 
+    print(f"========================================================================\n  {' '.join(comand)} - EXECUTED\n========================================================================\n")
+
 execute_annealing('0', 'False')
 execute_annealing('1', 'False')
 execute_annealing('2', 'False')
@@ -22,3 +27,12 @@ execute_annealing('0', 'True')
 execute_annealing('1', 'True')
 execute_annealing('2', 'True')
 
+execute_genetic('150', '35', '0.05')#MELHOR CASO#0.20
+execute_genetic('25', '35', '0.05')
+execute_genetic('150', '10', '0.05')
+execute_genetic('150', '35', '0.30')
+
+#execute_genetic('150', '35', '0.05')#MELHOR CASO#0.20
+#execute_genetic('25', '35', '0.05')
+#execute_genetic('150', '10', '0.05')
+#execute_genetic('150', '35', '0.30')
